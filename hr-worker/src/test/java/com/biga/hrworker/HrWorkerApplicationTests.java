ac67facd5159e3f2e40f4f8eded4a14dcbe9580b
Bighetto <arthurbighetto36@gmail.com>
2021-08-23 15:38:52 -0300

    Projeto Criado

---
+++ hr-worker/src/test/java/com/biga/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biga.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
